8e55edf91e48f0270369cb2bdb5f129ec7f2f9f2
Andrew Smith <als5ev@virginia.edu>
2018-08-11 23:38:39 -0400

    requirements.txt + gitignore

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
 import matplotlib.pyplot as plt
